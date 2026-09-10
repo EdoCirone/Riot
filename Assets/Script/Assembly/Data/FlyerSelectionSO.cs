0ef2a50d3e713e208e65b789caafa9cb543d3b0c
EdoCirone <edoardocirone@gmail.com>
2026-09-10 19:10:45 +0200

    FlyerSelectionSO

---
+++ Assets/Script/Assembly/Data/FlyerSelectionSO.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Selezione confermata nel Volantino.
+/// Conserva lo stato runtime condiviso tra Assemblea e livello.
+/// </summary>
+[CreateAssetMenu(
+    fileName = "FlyerSelectionSO",
+    menuName = "RIOT/Assembly/FlyerSelectionSO")]
+public sealed class FlyerSelectionSO : ScriptableObject
+{
+    private const int MinutesPerDay = 24 * 60;
+
+    [System.NonSerialized]
+    private ObjectiveSO _declaredObjective;
+
+    [System.NonSerialized]
+    private MeetingPointSO _meetingPoint;
+
+    [System.NonSerialized]
+    private int _startMinutesFromMidnight = -1;
+
+    public ObjectiveSO DeclaredObjective => _declaredObjective;
+    public MeetingPointSO MeetingPoint => _meetingPoint;
+    public int StartMinutesFromMidnight => _startMinutesFromMidnight;
+
+    public bool HasSelection =>
+        _declaredObjective != null
+        && _meetingPoint != null
+        && _startMinutesFromMidnight >= 0
+        && _startMinutesFromMidnight < MinutesPerDay;
+
+    public bool TrySetSelection(
+        ObjectiveSO declaredObjective,
+        MeetingPointSO meetingPoint,
+        int startMinutesFromMidnight)
+    {
+        if (declaredObjective == null
+            || meetingPoint == null
+            || startMinutesFromMidnight < 0
+            || startMinutesFromMidnight >= MinutesPerDay)
+        {
+            return false;
+        }
+
+        _declaredObjective = declaredObjective;
+        _meetingPoint = meetingPoint;
+        _startMinutesFromMidnight = startMinutesFromMidnight;
+
+        return true;
+    }
+
+    public void ClearSelection()
+    {
+        _declaredObjective = null;
+        _meetingPoint = null;
+        _startMinutesFromMidnight = -1;
+    }
+}
